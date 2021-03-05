760514e34af3d7a240104480f22e9201813a1960
liyeouwang <liwen4710@gmail.com>
2021-03-05 14:31:35 +0800

    modify SA.py

--- SA.py
+++ SA.py
@@ -1,6 +1,7 @@
 import numpy as np
 import math 
 import sys
+import copy
 
 T_MAX = 0
 
@@ -45,15 +46,12 @@ class Request:
 
 
 def accumulate_memory(request, servers, S_deliver, finish_time, catch_time):
-
 	#if(catch_time < 100000):
 	#print("request.ID: " + str(request.ID) + " finish_time: " + str(finish_time) + " catch_time: " + str(catch_time))
 	if(catch_time >= 10000):
 		return
 		#catch_time = T_MAX-1
 		#request.size = 100
-		#print("hi")
-
 	
 	for t in range(finish_time, catch_time):
 		#print("request.size: "+ str(request.size))
@@ -76,8 +74,6 @@ def wait_vehicle(request, S_deliver, finish_time):
 		if(vehicles[request.vehicle_id].range[S_deliver][t] == 1):
 			#print("request "+str(request.ID) + "vehicles["+ str(request.vehicle_id) +"].range["+str(S_deliver)+"]["+str(t)+"] == 1")
 			is_delivery = True
-
-			#print("catch")
 			break
 		#else:
 			#servers[S_deliver].memory[t] += request.size
@@ -106,6 +102,7 @@ def decide_S_deliver(servers, request, finish_compute_time):
 	request.S_deliver = T_list.index(min(T_list))
 	#print("request "+str(request.ID) + "T_list: " + str(T_list))
 	request.catch_time =  min(T_list)
+	#print("request.ID: " + str(request.ID) + " request.catch_time: " + str(request.catch_time ))
 	request.T_deliver = services[request.service_type].T_deliver[request.S_exe][request.S_deliver]
 
 	return request.S_deliver, request.catch_time 
@@ -119,8 +116,8 @@ def decide_priority_2(requests, servers, services, S_exe):
 	#for request in queue:
 	#	if(request.deadline > T_MAX):
 	#		request.deadline = T_MAX
-	#sorted_queue = sorted(queue, key=lambda x: x.size) #should be size or deadline?
-	sorted_queue = sorted(queue, key=lambda x: x.deadline)
+	sorted_queue = sorted(queue, key=lambda x: x.size) #should be size or deadline?
+	#sorted_queue = sorted(queue, key=lambda x: x.deadline)
 	#sorted_queue = sorted(queue, key = lambda x: (x.deadline, x.size)) #if same deadline, sort size 
 
 	for request in sorted_queue:
@@ -141,7 +138,6 @@ def decide_priority_2(requests, servers, services, S_exe):
 					request.shared = True
 				#print("S_exe: " + str(S_exe) + "request "+str(request.ID) + " service_type: " + str(service_type) + " share: " + str(request.shared))
 
-
 	T_wait = 0
 	for i in range(len(sorted_queue)):
 		if(sorted_queue[i].shared == True):
@@ -149,33 +145,19 @@ def decide_priority_2(requests, servers, services, S_exe):
 			#print("S_exe: " + str(S_exe) + "request "+str(sorted_queue[i].ID) + " shared with request " + str(sorted_queue[i].shared_with))
 		else:
 			if(T_wait < sorted_queue[i].deadline - sorted_queue[i].T_compute ):
-				'''
-				if(S_exe == 0):
-					T_wait = 35
-				else:
-					T_wait = 36
-				'''
 				T_wait = np.random.randint(T_wait, sorted_queue[i].deadline - sorted_queue[i].T_compute) 
 				#print("sorted_queue[i].deadline: " + str(sorted_queue[i].deadline))
 				#print("T_wait: "+ str(T_wait))
 			#sys.exit()
 			#sorted_queue[i].priority = i 
 			sorted_queue[i].T_wait = T_wait  
-			T_wait += sorted_queue[i].T_compute
-
-		
+			T_wait += sorted_queue[i].T_compute	
 		#print("S_exe: " + str(S_exe) + " request.ID: " + str(sorted_queue[i].ID) + " T_wait: " + str(sorted_queue[i].T_wait))
 		#print("S_exe: " + str(S_exe) + "sorted_queue[i].priority: " + str(sorted_queue[i].priority))
-
 		#print("S_exe: " + str(S_exe) + "sorted_queue[i].T_compute: " + str(sorted_queue[i].T_compute))
 
-	#for service in sorted_queue:
-	#	if(service.T_wait != 0):
-	#		print("service.T_wait" + str(service.T_wait))
-	
 	return sorted_queue
 def find_sol_2(services, requests, servers, T_MAX):
-
 	#refresh the server
 	for server in servers:
 		server.memory = [0] * T_MAX
@@ -190,7 +172,6 @@ def find_sol_2(services, requests, servers, T_MAX):
 			start_exe_time = earliest_start_time + request.T_wait
 			#print("start_exe_time: " + str(start_exe_time) + "deadline: " + str(request.deadline))
 			if(start_exe_time >= request.deadline):
-				#print("out")
 				all_max_memory_use = 10000
 				continue		
 			finish_compute_time = start_exe_time + request.T_compute
@@ -200,7 +181,6 @@ def find_sol_2(services, requests, servers, T_MAX):
 			#print("request.ID: " + str(request.ID) + " S_deliver: " + str(S_deliver))
 			finish_time = finish_compute_time  + services[request.service_type].T_deliver[S_exe][S_deliver]
 			#print("finish_time: " + str(finish_time) + " catch_time: " + str(catch_time))
-			#print("finish_time: " + str(finish_time))
 			accumulate_memory(request, servers, S_deliver, finish_time, catch_time)
 	#sum up memory usage 
 
@@ -226,9 +206,7 @@ def get_init_sol(requests):
 	for r in range(len(requests)):
 		k = requests[r].service_type
 		requests[r].S_exe = 0
-
 		servers[requests[r].S_exe].exe_queue.append(r)
-		#requests[r].S_deliver = 0
 		requests[r].T_request = services[k].T_request[requests[r].S_exe]
 		requests[r].T_compute = services[k].T_compute[requests[r].S_exe]
 		#print("requests[r].T_request: " + str(requests[r].T_request))
@@ -259,16 +237,12 @@ def read_inputs(services, servers, vehicles, requests):
 	for k in range(num_services):
 		services.append(Service(k, num_servers, num_vehicles))
 
-	
-
-
 	for j in range(num_servers):
 		tmp = [int(x) for x in input().split()]
 		for k in range(num_services):
 			services[k].T_request[j] = tmp[k]
 			
 			#sys.exit()
-	
 
 	for j in range(num_servers):
 		T_computes = [int(x) for x in input().split()]
@@ -296,7 +270,6 @@ def read_inputs(services, servers, vehicles, requests):
 			print("\n") 
 		print("\n") 	
 	'''		
-
 
 	num_requests = 0
 	for i in range(num_vehicles):
@@ -346,69 +319,26 @@ def read_inputs(services, servers, vehicles, requests):
 		servers[j].capacity = capacities[j]
 		#print(servers[j].capacity)
 
-
-	
-
-	'''
-	input_file = open("input.txt")
-	
-	#num_servers = int(input_file.readline())
-	
-
-	#service 
-	num_services = int(input_file.readline())
-	for k in range(num_services):		
-		line = input_file.readline().split()
-
-		#vehicle_id, service_type, size, deadline
-		service = Service(int(line[0]), int(line[1]), int(line[2]), int(line[3]))
-		
-		line = input_file.readline()
-		service.T_request = [int(i) for i in line.split()]
-
-		line = input_file.readline()
-		service.T_compute = [int(i) for i in line.split()]
-
-		line = input_file.readline()
-		service.T_deliver = [int(i) for i in line.split()]
-
-		services.append(service)
-
-	input_file.close()
-
-	input_file = open("paths.txt")
-	num_vehicles = int(input_file.readline())
-	for v in range(num_vehicles):
-		vehicle_id = int(input_file.readline())
-		for s in range(num_servers):
-			line = input_file.readline().split()
-			servers[s].t_vehicle_arrive[vehicle_id] = int(line[0])
-			servers[s].t_vehicle_leave[vehicle_id] = int(line[1])
-			
-	input_file.close()
-	'''
-
 if __name__ == '__main__':
-
-	
-
 	servers = []	
 	services = []
 	vehicles = []
 	requests = []
 	#read input file	
 	read_inputs(services, servers, vehicles, requests)
-	#print("T_MAX: " + str(T_MAX))
 	init_requests(requests, services)
 	get_init_sol(requests)
 	
 	cur_sol, requests = find_sol_2(services, requests, servers, T_MAX)
 	
 	best_sol = cur_sol
-	best_requests = requests
+	best_requests = copy.deepcopy(requests)
+	# remind: python passes lists by reference! requests changes, best_requests changes, too 
+	# so, use deepcopy to copy by value 
+
 	#print("init_sol = " + str(best_sol))
 	#sys.exit()
-	#print("\n")
+
 
 	T = 100000
 	r = 0.999
@@ -423,28 +353,22 @@ if __name__ == '__main__':
 			cur_sol = new_sol
 			if(cur_sol < best_sol):
 				best_sol = cur_sol
-				best_requests = requests
+				best_requests = copy.deepcopy(requests)
 				#print("best_sol = " + str(best_sol))
 				#if(best_sol == 0):
-				#	sys.exit()
+					#print("best")
+					#break
 		else:
 			if(np.random.rand() <= math.exp(- delta_cost / T )):
 				cur_sol = new_sol
-
 		T *= r
 
-	f = open("verify.in", "w")
+	#print("best_sol = " + str(best_sol))
+	print(best_sol)
 
-	print("best_sol = " + str(best_sol))
-	f.write(str(best_sol))
-	f.write("\n")
 	for request in best_requests: 
-		f.write(f"{request.S_exe} {request.S_deliver} {request.T_wait} {request.catch_time}")
 		print(f"{request.S_exe} {request.S_deliver} {request.T_wait} {request.catch_time}")
-		#print(f"{request.S_deliver} ")
-		#print(f"{request.T_wait} ")
-		#print(f"{request.catch_time} ")
-		f.write("\n")
-	print("count: " + str(count))
-	#get a initial solution
+
+	#print("count: " + str(count))
+
 	sys.exit()
